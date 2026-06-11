dffdadc29216958ebbb95517b455474e4fba3200
Kau <ks5356014@gmail.com>
2026-06-11 11:51:52 -0300

    cardapio digital

---
+++ script.js
@@ -0,0 +1,47 @@
+abrirModalAbobora = document.getElementById("abobora");
+modalAbobora = document.getElementById("doce_abobora");
+fecharModalAbobora = document.getElementById("botao_abobora");
+
+abrirModalAbobora.addEventListener("click", function() {
+    modalAbobora.showModal();
+});
+
+fecharModalAbobora.addEventListener("click", function() {
+    modalAbobora.close();
+});
+
+abrirModalBolo = document.getElementById("bolo");
+modalBolo = document.getElementById("bolo_coberto");
+fecharModalBolo = document.getElementById("botao_bolo");
+
+abrirModalBolo.addEventListener("click", function() {
+    modalBolo.showModal();
+});
+
+fecharModalBolo.addEventListener("click", function() {
+    modalBolo.close();
+});
+
+abrirModalBemCasado = document.getElementById("Bem_casado");
+modalBemCasado = document.getElementById("bem_casado");
+fecharModalBemCasado = document.getElementById("botao_bem_casado");
+
+abrirModalBemCasado.addEventListener("click", function() {
+    modalBemCasado.showModal();
+});
+
+fecharModalBemCasado.addEventListener("click", function() {
+    modalBemCasado.close();
+});
+
+abrirModalMixDoces = document.getElementById("Mix_doces");
+modalMixDoces = document.getElementById("mix_doces");
+fecharModalMixDoces = document.getElementById("botao_mix_doces");
+
+abrirModalMixDoces.addEventListener("click", function() {
+    modalMixDoces.showModal();
+});
+
+fecharModalMixDoces.addEventListener("click", function() {
+    modalMixDoces.close();
+});
